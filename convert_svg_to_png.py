c5f8eb879fb17d2e635366ba4d3648a04d5d37ca
pleabargain <dennisgdaniels@gmail.com>
2021-12-27 11:33:39 +0200

    cleaning up directories

--- convert_svg_to_png.py
+++
@@ -1,9 +0,0 @@
-# resource
-# https://clay-atlas.com/us/blog/2021/03/08/python-en-svglib-convert-svg-png/
-
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
-
-# TODO loop over the entire directory and parse only the SVG
-drawing = svg2rlg('/home/dgd/Desktop/python_storyboard_flashcards/random_images/bikini.svg')
-renderPM.drawToFile(drawing, '/home/dgd/Desktop/python_storyboard_flashcards/random_images/bikini.png', fmt='PNG')
