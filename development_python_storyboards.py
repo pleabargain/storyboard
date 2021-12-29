341ee583cc3ed8716183d6b6bfc5aa73a7f8c4bd
pleabargain <dennisgdaniels@gmail.com>
2021-12-29 12:52:25 +0200

    more updates

--- development_python_storyboards.py
+++ development_python_storyboards.py
@@ -287,7 +287,7 @@ menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
 
 timeline_column_one = sg.Column([
                                 [sg.Text("timeline column one",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 key="past1",
                                 size = (20,2),
@@ -296,7 +296,7 @@ timeline_column_one = sg.Column([
 
 ###
                                 [sg.Text("past2 column one",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 key="past2",
                                 size = (20,2),
@@ -304,7 +304,7 @@ timeline_column_one = sg.Column([
                                 tooltip='Past events2')], 
 ###
                                 [sg.Text("past3",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 key="past3",
                                 size = (20,2),
@@ -312,7 +312,7 @@ timeline_column_one = sg.Column([
                                 tooltip='past3 column one')],
 ###
                                 [sg.Text("now column one",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 key="now_event",
                                 size = (20,2),
@@ -321,7 +321,7 @@ timeline_column_one = sg.Column([
 ###
                                 [sg.Text("timeline column one",
                                 key="future1",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (20,2),
                                 enable_events=True,
@@ -329,7 +329,7 @@ timeline_column_one = sg.Column([
 ###
                                 [sg.Text("timeline column one",
                                 key="future2",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (20,2),
                                 enable_events=True,
@@ -337,7 +337,7 @@ timeline_column_one = sg.Column([
 ###
                                 [sg.Text("timeline column one",
                                 key="future3",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (20,2),
                                 enable_events=True,
@@ -348,9 +348,10 @@ timeline_column_one = sg.Column([
 
 
 timeline_column_two = sg.Column([
+                                [sg.Button("edit timeline events") ],
                                 [sg.Text("timeline column one",
                                 key="event1",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -359,7 +360,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("past2 column one",
                                 key="event2",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -367,7 +368,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("eventpast3",
                                 key="event3",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -375,7 +376,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("now column one",
                                 key="event4",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -383,7 +384,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("timeline column one",
                                 key="event5",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -391,7 +392,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("event 6 timeline column one",
                                 key="event6",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -399,7 +400,7 @@ timeline_column_two = sg.Column([
 ###
                                 [sg.Text("timeline column one",
                                 key="event7",
-                                font = ("helvetica", 14),
+                                font = ("helvetica", 12),
                                 justification = "center",
                                 size = (30,2),
                                 enable_events=True,
@@ -409,8 +410,11 @@ timeline_column_two = sg.Column([
 
 
 timeline_column_three = sg.Column([
+                                [sg.Button("edit adverbs of time phrases") ],
                                 [sg.Text("timeline column one",
                                 key="adverb1",
+                                font = ("helvetica", 12),
+
                                 size = (30,2),
                                 enable_events=True,
                                 tooltip='This is a tool tip for adverb1')],
@@ -418,6 +422,7 @@ timeline_column_three = sg.Column([
 ###
                                 [sg.Text("past2 column one",
                                 key="adverb2",
+                                font = ("helvetica", 12),
                                 size = (30,2),
                                 enable_events=True,
                                 tooltip='This is a tool tip for adverb2')], 
@@ -425,32 +430,37 @@ timeline_column_three = sg.Column([
                                 [sg.Text("eventpast3",
                                 key="adverb3",
                                 size = (30,2),
+                                font = ("helvetica", 12),
                                 enable_events=True,
                                 tooltip='This is a tool tip timeline column one')],
 ###
                                 [sg.Text("now column one",
                                 key="adverb4",
                                 size = (30,2),
+                                font = ("helvetica", 12),
                                 enable_events=True,
                                 tooltip='This is a tool tip timeline column one')],
 ###
                                 [sg.Text("timeline column one",
                                 key="adverb5",
                                 size = (30,2),
+                                font = ("helvetica", 12),
                                 enable_events=True,
-                                tooltip='This is a tool tip timeline column one')],
+                                tooltip='This is a tool tip for adverb5')],
 ###
                                 [sg.Text("timeline column one",
                                 key="adverb6",
                                 size = (30,2),
+                                font = ("helvetica", 12),
                                 enable_events=True,
-                                tooltip='This is a tool tip timeline column one')],
+                                tooltip='This is a tool tip for adverb6')],
 ###
                                 [sg.Text("timeline column one",
                                 key="adverb7",
                                 size = (30,2),
+                                font = ("helvetica", 12),
                                 enable_events=True,
-                                tooltip='This is a tool tip timeline column one')],
+                                tooltip='This is a tool tip for adverb7')],
 
                                 ])
 
@@ -676,7 +686,7 @@ tab_one= sg.Tab ("adj noun reg verb", [
             sg.Listbox(adjectives_list,key="adjectives_list_box",enable_events=True,change_submits=True,size=(15,15)),
             sg.Listbox(nouns_list,key="nouns_list_box",enable_events=True,change_submits=True,size=(15,15)),
         ],
-            [sg.Multiline(key="simple_sentence_builder_output",size =(50,5),default_text= mermaid_template,   tooltip="simple_sentence_builder_output"), ],
+            [sg.Multiline(key="simple_sentence_builder_output",size =(50,5), font = ("helvetica",14),default_text= mermaid_template,   tooltip="simple_sentence_builder_output"), ],
             [sg.Button("reload"),sg.Button("randomize",tooltip="click to randomize"),],
             [sg.Button("save your created sentence",tooltip="save your work to a text file"),],
         
@@ -685,7 +695,7 @@ tab_one= sg.Tab ("adj noun reg verb", [
 
     )
 
-tab_two= sg.Tab ("tenses tab", [
+storyboard_tenses_tab_two= sg.Tab ("storyboard tenses tab", [
         #create button
         [sg.Button("shuffle the images",
                 key = "image_shuffle",
@@ -710,7 +720,7 @@ tab_two= sg.Tab ("tenses tab", [
 #### negotiation TAB
 #TODO add edit button so I can quickly go in and add entries
 
-tab_three = sg.Tab("negotiation",
+negotiation_tab_three = sg.Tab("negotiation",
 [
         
 [
@@ -906,9 +916,9 @@ timeline_tab= sg.Tab ("timeline tenses tab",
     [
       
         #create button
-        [sg.Button("change time",tooltip="click to change all of the times"), sg.Button("randomize events"),   ],
+        [sg.Button("change time",tooltip="click to change all of the times"), sg.Button("randomize timeline events"),   ],
     
-        [timeline_column_one,timeline_column_two],
+        [timeline_column_one,timeline_column_two, timeline_column_three],
     
     ] 
     )
@@ -983,13 +993,13 @@ layout = [
     # [sg.Canvas(size=(500, 200), key='canvas')],
     #done use image resizer on images
     [sg.Text("student name:"),sg.InputText("student_name",key="student_name",tooltip="student_name goes here",justification="top")],
-    [sg.TabGroup([[tab_one,tab_two,tab_three,timeline_tab, pros_cons_tab]],key="tabgroup"),],
+    [sg.TabGroup([[tab_one,storyboard_tenses_tab_two,negotiation_tab_three,timeline_tab, pros_cons_tab]],key="tabgroup"),],
    
 ]
     
 
 
-window = sg.Window('Development! Learn English with Dennis', 
+window = sg.Window('Development '+ datetime.date.today().strftime("%Y %B %d %A") + ' Dennis Helps Your Career with English', 
                     
                     layout, 
                     background_color="lightblue",
@@ -1398,7 +1408,7 @@ while True:
         future3 =   future2 + datetime.timedelta(delta3)
         window["future3"].update(future3.strftime("%Y %B %d %A"))
 
-    if event == "randomize events":
+    if event == "randomize timeline events":
         with open("/home/dgd/Desktop/python_storyboard_flashcards/timeline_support_tab/timeline_events.md") as myfile:
             lines = myfile.readlines()
         random.shuffle(lines)
--- timeline_support_tab/timeline_events.md
+++ timeline_support_tab/timeline_events.md
@@ -1,103 +1,112 @@
-start building a new house
-Rammstein Concert
-move from my home town
-born
+3D print a piece of art
 birth of first child
 birth of second child
-buy an apartment
-Football Champion 
-buy a house
-read the entire Bible
-read the entire Koran
-read the entire Mahabharata
-to get certified
-to get a drivers license
-sing karaoke
-make sushi
-eat oysters
-make homemade beer
-make homemade wine
-first time driving a car
-white water rafting
+born
 build a kit car
 build a kit plane
-fly a drone
-parachute from a plane
 buy a car
-car accident
 buy a Christmas tree
-decorate a Christmas tree
-to marry
-to donate blood
-to plant a tree
-to rent a car
-to have surgery
-to get a vaccine
-to slip on ice
-to visit an old friend
-visit the opera
-to build an application
-plan for New Years
-graduate from high school
-graduate from university
-move in with your significant other
-go surfing
-go sailing
-go kayaking
-visit my grandmother's birthplace
-go SCUBA diving
-go snorkeling
-go wake boarding
-go swimming 
-go soak in the jacuzzi
-go to school
-watch a lunar eclipse
-watch a solar eclipse
-travel to Mallorca
-go skiing
-go water skiing
-go to a meeting
-go to a conference
-give a lecture
-select a vendor
+buy a house
+buy an apartment
+car accident
 create a course
+decorate a Christmas tree
+drive a tractor
+eat oysters
 finish the project
-ride a horse on the beach
 first kiss
 first love
 first smart phone
 first snow
+first time driving a car
+first time living alone
+first time shaving
+first visit to the barber
 first visit to the circus
 first visit to the dentist
-first visit to the barber
-first time shaving
-play poker
-play chess
-learn to play the piano
-first time living alone
-party with friends
-have a vacation
+fly a drone
+Football Champion
 get robbed
-go on vacation
-go skiing in the Alps
-go to the mountains
-go to New Years Party
-go to Times Square
+give a lecture
 go Christmas carolling
+go kayaking
+go on vacation
+go sailing
+go SCUBA diving
+go skiing
+go skiing in the Alps
+go snorkeling
+go soak in the jacuzzi
+go surfing
+go swimming
 go to a concert
+go to a conference
+go to a meeting
+go to New Years Party
+go to school
 go to the dentist
 go to the hospital
-publish a book
+go to the mountains
+go to the zoo
+go to Times Square
+go wake boarding
+go water skiing
+graduate from high school
+graduate from university
+have a vacation
+kill a chicken
+learn to play the piano
+make dinner with the family
+make homemade beer
+make homemade wine
+make sushi
+meet enemy
+meet spouse
+move from my home town
+move in with your significant other
+own a cat
+own a dog
+parachute from a plane
+party with friends
 perform on stage
+plan for New Years
+play chess
+play in the snow
+play poker
+publish a book
+Rammstein Concert
+read a book
+read the entire Bible
+read the entire Koran
+read the entire Mahabharata
+ride a horse on the beach
+select a vendor
+sing karaoke
+start building a new house
+stuck in traffic
 take a course
 take a walk in the forest
-read a book
-meet spouse
-meet enemy
-own a dog
-own a cat
+to build an application
+to donate blood
+to get a drivers license
+to get a vaccine
+to get certified
+to have surgery
+to marry
+to plant a tree
+to rent a car
+to slip on ice
+to visit an old friend
 travel overseas
 travel to Italy
-win a contest
+travel to Mallorca
+visit my grandmother's birthplace
+visit the opera
+watch a lunar eclipse
+watch a solar eclipse
+watched a rocket launch
+white water rafting
 win a competition
+win a contest
 write a book
+sell a car
